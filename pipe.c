a6c5bd3b34f2d993e5d45ead6e0bde858050facb
Sanjiv <sanjiv.ch09@gmail.com>
2016-11-29 15:45:02 +0530

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -2,20 +2,11 @@
 #include<conio.h>
 #include<math.h>
 
-void main()
+int main()
 {
-	float a0,a1,a2,ratedspeed, nomspeed, speedfactor,head, flow;
+	float din,dout,epsilon,length,areaflow;
 	
-	scanf("%f %f",&ratedspeed, &nomspeed);
-	speedfactor=nomspeed/ratedspeed;
-	
-	scanf("%f %f %f", &a0, &a1, &a2);
-	scanf("%f",&flow);
-	
-	head=a0*speedfactor*speedfactor+a1*speedfactor*flow+a2*flow*flow;
-	
-	printf("Head:%f",head);
-	
-	return 0;	
+	//Area calculation
+	areaflow=3.14*din*din/4;
 	
 }
