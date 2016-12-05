80c30b18db3542b1ec18ddc47e328608fc435403
Sanjiv <sanjiv.ch09@gmail.com>
2016-12-05 08:45:32 +0530

    Create tank.c

---
+++ tank.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#define PI 3.1415
+void main()
+{
+float tankHeight,diaInner, diaOuter, thickness, tankVolume;
+int tankShape;
+
+
+diaOuter=diaInner + thickness;
+
+tankVolume=PI*diaInner*diaInner*tankHeight/4;
+
+}
