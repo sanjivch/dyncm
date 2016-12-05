df1040ee45707dbc34a84d757dd12d8fcb51e1d1
Sanjiv <sanjiv.ch09@gmail.com>
2016-12-05 15:16:25 +0530

    Update presflo2.c
    Updated variable names; cleaned some code

--- presflo2.c
+++ presflo2.c
@@ -3,21 +3,21 @@
 /* 																			  */
 /*functiontwo:																  */
 /*																			  */
-/*numFixedGrNode-NUMBEROFFIXEDGRADENODEintESYSTEM									  */
-/*JJ-COUNTERTOCHECKTHENUMBEROFJUNCTION                                        */
-/*KN:NOFJunctionintheSystem                                                  */
+/*numFixedGrNode-NUMBER OF FIXED GRADE NODE in the SYSTEM									  */
+/*countJn-COUNTER TO CHECK THE NUMBER OF JUNCTION                                        */
+/*numJunctions:NO of Junction in the System                                                  */
 /*JA-JB:JUNCTIONCONNECTEDTHEPIPe                                              */
-/*IP[][]-MATRIXFOREACHJUNCTIONint HESYSTEM,									  */
-/*EMIN[]-GRADEWHICHTHEPRVSET												  */
+/*IP[][]-MATRIX FOR EACH JUNCTION IN THES YSTEM,									  */
+/*EMIN[]-GRADE WHICH THE PRV SET												  */
 /*LZ[]-PIPENUMBERDOWENSTREAMFROMTEHFIRSTPRV									  */
-/*NOP-NUMBEROFPIPEAROUNDEACHJUNCTION										  */
-/*NEL-NUMBEROFPIPEAROUNDALLTHEJUNCTIONint HESYSTEM							  */
-/*KJ-KN-1..NUMBEROFJUNCTION-1												  */
-/*KP-KK-KN...NUMBEROFPIPE-NUMBEROFJUNCTION									  */
+/*numPipeAroundEachJn-NUMBEROFPIPEAROUNDEACHJUNCTION										  */
+/*numPipeAroundAllJn-NUMBEROFPIPEAROUNDALLTHEJUNCTIONint HESYSTEM							  */
+/*KJ-numJunctions-1..NUMBEROFJUNCTION-1												  */
+/*KP-numPipes-numJunctions...NUMBEROFPIPE-NUMBEROFJUNCTION									  */
 /*LNUM-NUMBEROFJUNCTION														  */
-/*KK-Nnumberofpipes														      */
+/*numPipes-Nnumberofpipes														      */
 /*																			  */
-/*fileNmaemonain.monao														  */
+
 /*===========================================================================*/
 #include <stdio.h>
 #include <math.h>
@@ -33,12 +33,12 @@ FILE *fili,*filo,*fill;
 
 void main(void)
 {
-	int KN,KK,NR,numFixedGrNode;
+	int numJunctions,numPipes,NR,numFixedGrNode;
 	int J99=30;
-	int LZ[MAXPRV],NUMJ[10];
+	int LZ[MAX_PRV],NUMJ[10];
 	int IP[MAX_PIP][13],M[MAX_PIP],JA[MAX_PIP],JB[MAX_PIP];
-	int MPL[MAX_SIZ],JX[MAX_SIZ],JIJ[MAX_SIZ],KIP[MAX_PIP],KPI[MAX_PIP],JJI[MAX_PIP];
-	int JJ,NEL,NOP,J1,J2,MMM,JMAX;
+	int arrPipeJn[MAX_SIZ],JX[MAX_SIZ],arrNumJn[MAX_SIZ],KIP[MAX_PIP],KPI[MAX_PIP],JJI[MAX_PIP];
+	int countJn,numPipeAroundAllJn,numPipeAroundEachJn,J1,J2,MMM,JMAX;
 	int KTEP;
 	int ERR=0;
 	int j,m,h,v,t,g,k,i;
@@ -46,32 +46,32 @@ void main(void)
 	char fnaml[20],fnam2[20];
 	
 	/*============================================================================*/
-	printf("\Givefilenameforinput\r\n");
+	printf("Givefilenameforinput\r\n");
 	scanf("%s",fnaml);
 	
 	if((fili=fopen(fnaml,"r+"))==NULL)
 	{
-		printf("Cannotopeninputf.%s",fnaml);
+		printf("Cannot open input f.%s",fnaml);
 		exit(0);
 	}
 	
-	printf("Givefilenameforoutput\r\n");
+	printf("Give filename for output\r\n");
 	scanf("%s",fnam2);
 	
 	if((filo=fopen(fnam2,"a+"))==NULL)
 	{
-		printf("Cannotopenoutputf.%s",fnam2);
+		printf("Cannot open output f.%s",fnam2);
 		exit(0);
 		
 		//======================================================
-		fscanf(fili,"%d%d%d%d",KN,KK,NR,numFixedGrNode);
+		fscanf(fili,"%d %d %d %d",numJunctions,numPipes,NR,numFixedGrNode);
 		
-		for(j=l;j<=KK;++j)
+		for(j=l;j<=numPipes;++j)
 		{
-			fscanf(fili,"%d%d%d%d%d",JA[j],JB[j],KPI[j],KIP[j],MPL[j]);
+			fscanf(fili,"%d %d %d %d %d",JA[j],JB[j],KPI[j],KIP[j],arrPipeJn[j]);
 		}
 		
-		for(i=l;i<=KN;++i)
+		for(i=1;i<=numJunctions;++i)
 		{
 			fscanf(fili,"%d%d",NUMJ[i],JX[i]);
 		}
@@ -82,8 +82,8 @@ void main(void)
 		}
 		
 		KTEP=numFixedGrNode-1;
-		JJ=0;
-		NEL=0;
+		countJn=0;
+		numPipeAroundAllJn=0;
 	}
 	
 	for(j=1;j<=99;++j)
@@ -91,28 +91,28 @@ void main(void)
 		//--------------------------->>j
 		if(JX[j]!=0)
 		{
-			//ifJX[j]=0 means fiexd grade node
+			//ifJX[j]=0 means fixed grade node
 			JMAX=j;
-			JJ=JJ+1;
-			fprintf(filo,"\nJJ=%d",JJ);
-			JJI[JJJ=j;
-			fprintf(filo,"\nJJX[%d]=%d",JJ,j);
-			JIJ[j]=JJ;
-			fprintf(filo,"\nJIJ[%d]=%d",j,JJ);
-			printf("\n-----------marix-------------------=");
+			countJn=countJn+1;
+			fprintf(filo,"\nJJ=%d",countJn);
+			JJI[countJn]=j;
+			fprintf(filo,"\nJJX[%d]=%d",countJn,j);
+			arrNumJn[j]=countJn;
+			fprintf(filo,"\nJIJ[%d]=%d",j,countJn);
+			printf("\n-----------matrix-------------------=");
 			scanf("%d",&ERR);
 		}
 	}//-----------------------------------------------«j
 	
-	if(JJ!=KN)
+	if(countJn!=numJunctions)
 	{
-		//JJisCOUNTERtoCHECKthenumberofjunction
-		KN=JJ;
+		//countJn is COUNTER to CHECK the number of junction
+		numJunctions=countJn;
 	}
 	
-	for(j=1;j<=KK;++j)
+	for(j=1;j<=numPipes;++j)
 	{
-		JJ=KPI[j];
+		countJn=KPI[j];
 		fprintf(filo,"\nJJ=%d",KPI[j]);
 		J1=0;
 		J2=0;
@@ -120,12 +120,12 @@ void main(void)
 		{
 			t=JA[j];
 			Jl=Jl+JXJ[t];
-			fprintf(filo,"\nJl=%d",JIJ[t]);
+			fprintf(filo,"\nJl=%d",arrNumJn[t]);
 		}
 		if(JB[j]!=0)
 		{
 			g=JB[j];
-			J2=J2+JIJ[g]i
+			J2=J2+arrNumJn[g]i
 			fprintf(filo,"\nJ2=%d",JlJ[g]);
 		}
 		if(Jl!=0)
@@ -143,15 +143,15 @@ void main(void)
 		if(J2!=0)
 		{
 			M[J2]=M[J2]+1;
-			fprintf(filo,"\nM[%d]=%d",J2,M[J2]);
+			fprintf(filo,"\n M[%d]=%d",J2,M[J2]);
 			MMM=M[J2];
-			fprintf(filo,,l\nMMM=%d",MMM);
+			fprintf(filo,"\n MMM=%d",MMM);
 			IP[J2][MMM]=-j;
-			fprintf(filo,"\nIP[%d][%d]=-%d",J2,MMM,j);
+			fprintf(filo,"\n IP[%d][%d]=-%d",J2,MMM,j);
 			printf("\n---------MATRIX[]----------=",ERR);
 			scanf("%d",&ERR);
 		}
-		if(MPL[JJ]==101)
+		if(arrPipeJn[countJn]==101)
 		{
 			JA[j]=0;
 		}
@@ -161,45 +161,45 @@ void main(void)
 	{
 		for(j=1;j<=NR;++j)
 		{
-			v=LZ[j];//LZ[j]=pipenumberdowenstreamfromfirstprv
-			JJ=KIP[v];
-			ENGY[JJ]=EMIN[j];//theenrgyforthispipeisequaltogradewhichprvset
+			v=LZ[j];//LZ[j]=pipe number downstream from first prv
+			countJn=KIP[v];
+			ENGY[countJn]=EMIN[j];//the energy for this pipe is equal to grade with which prv is set
 		}
 	}
 	
-		for(j=1;j<=KN;++j)
+		for(j=1;j<=numJunctions;++j)
 		{		//--------------------->>>>>>>>>>>>M
-				NOP=M[j];//sumofthepipearoundeachjunction
-				fprintf(filo,"\nNOP=%d",NOP);
-				if(NOP!=0)
+				numPipeAroundEachJn=M[j];//sum of the pipe around each junction
+				fprintf(filo,"\nNOP=%d",numPipeAroundEachJn);
+				if(numPipeAroundEachJn!=0)
 				{
-					for(k=l;k<=NOP;++k)
+					for(k=1;k<=numPipeAroundEachJn;++k)
 					{
-						NEL=NEL+1;//thesumofallthepipeinalljunctionint hesystem
-						fprintf(filo,"\nNEL=%d",NEL);
-						MPL[NEL]=IP[j][k];//settingthematrix
-						fprintf(filo,"\nMPL[%d]=IP[%d][%d]",NEL,j,k);
+						numPipeAroundAllJn=numPipeAroundAllJn+1;//the sum of all the pipe in all junction in the system
+						fprintf(filo,"\nNEL=%d",numPipeAroundAllJn);
+						arrPipeJn[numPipeAroundAllJn]=IP[j][k];//setting the matrix
+						fprintf(filo,"\nMPL[%d]=IP[%d][%d]",numPipeAroundAllJn,j,k);
 					}
-				M[j]=NEL-N0P+1;
-				fprintf(filo,"\nM[%d]=%d-%d+l",j,NEL,NOP);
+				M[j]=numPipeAroundAllJn-numPipeAroundEachJn+1;
+				fprintf(filo,"\nM[%d]=%d-%d+l",j,numPipeAroundAllJn,numPipeAroundEachJn);
 				printf("\n---------matrix----------=%d",ERR);
 				scanf("%d",&ERR);
 				}
 		}
 		
-		M[KN+1]=NEL+1;
+		M[numJunctions+1]=numPipeAroundAllJn+1;
 		
-	for(j=1;j<=KK;++j)
+	for(j=1;j<=numPipes;++j)
 	{
 		if(JA[j]!=0)
 		{
 			m=JA[j];
-			JA[j]=JIJ[m];
+			JA[j]=arrNumJn[m];
 		}
-		if(JB[j]1=0)
+		if(JB[j]!=0)
 		{
 			h=JB[j];
-			JB[j]=JIJ[h];
+			JB[j]=arrNumJn[h];
 		}
 	}
 	fprintf(filo,"\nKTEP=%dJMAX=%d",KTEP,JMAX);
