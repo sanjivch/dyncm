93e90f04208b3e9077552a98609b32300a732c51
Sanjiv <sanjiv.ch09@gmail.com>
2016-12-07 15:03:47 +0530

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -5,7 +5,8 @@
 
 void main()
 {
-	float diaInner,diaOuter,epsilon,pipeLength, fitResistance;
+	float diaInner,diaOuter,epsilon,pipeLength, fitResistance, K1, Kinf, NRe;
+	float viscosity, refViscosity, opTemp,refTemp;
 	int fitResType;
 	float flowArea;
 	float flowVelocity,fluidDensity,massFlow,volFlow,resTime;
@@ -22,7 +23,17 @@ void main()
 	//Residence time - should be more than 1 sec to avoid computational leaks
 	resTime=pipeLength/flowVelocity;
 	
-	//fitResType -Fitting Resistance type - 0=2K method, 1=3K method, 2=L/D method (Refer Crane TP410)
+	//Reynolds number
+	//Viscosity needs to be calculated at the operating tempwerature from the power law mu/muo=(T/To)^0.7 Ref: http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
+	viscosity=refViscosity*(opTemp/refTemp)^0.7;
+	NRe=diaInner*flowVelocity*fluidDensity/viscosity;
+	
+	//fitResType -Fitting Resistance type - 0=2K method, 1=3K method, 2=L/D method (Ref: Crane TP410)
 	fitResistance=1000.0;
+	
+	fitResistance=K1/NRe +Kinf(1+1/diaInner);//2K method
+	//In this Equation K∞ is the "classic" K for a large fitting in the fully turbulent flow regime and K1 is the resistance coefficient at a Reynolds Number of 1. Note that although the K's and Re are dimensionless the fitting inside diameter (D) must be given in inches. Ref: http://www.katmarsoftware.com/articles/pipe-fitting-pressure-drop.htm
+	
+	
 	
 }
