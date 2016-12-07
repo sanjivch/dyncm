1ff326284172360b923386fb2ad3b7370f4d11b2
Sanjiv <sanjiv.ch09@gmail.com>
2016-12-07 14:06:31 +0530

    Update pidcontroller.c
    added functionality

--- pidcontroller.c
+++ pidcontroller.c
@@ -5,13 +5,42 @@
 void main()
 {
 
-  float processVar,setVar,error,remSet, gain, integral, derivative;
+	float processValue, setPoint, error, previousError, remSetPoint, controllerOut, epsilon=0.001, Kp, Ki, Kd, integral, derivative;//epsilon is the tolerance and is decided by the user
+	float time, simTime, deltaT=0.01;//deltaT to be decided by the user
+	
+	
+	for(time=0.0; time=simTime; time+=deltaT)
+	{
+		error=setPoint-processValue;
   
-  
-  error=processVar-setVar;
-  
-  
+	
+		//Incaseof error too small then stop integration
+		if(abs(error)> epsilon)
+		{
+		integral += error*deltaT;
+		}
+		
+		derivative = (error -previousError)/deltaT;
+		
+		controllerOut = Kp*error+ Ki*integral+ Kd*derivative;
+		
+		//Saturation Filter
+		/*if(output> MAX)
+		{
+		output= MAX;
+		}
+		elseif(output< MIN)
+		{
+		output= MIN;
+		}*/
+		//Update error
+		previousError= error;
+	}
+	
+	
+}
+
   
   
 
-}
+
