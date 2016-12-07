976939b697c8c9942d8de5697aad58fff564a0a0
Sanjiv <sanjiv.ch09@gmail.com>
2016-12-07 10:30:26 +0530

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -3,18 +3,26 @@
 #include<math.h>
 #define PI 3.1415
 
-int main()
+void main()
 {
-	float diaInner,diaOuter,epsilon,pipeLength,flowArea,flowVelocity,fluidDensity,massFlow,volFlow,resTime;
+	float diaInner,diaOuter,epsilon,pipeLength, fitResistance;
+	int fitResType;
+	float flowArea;
+	float flowVelocity,fluidDensity,massFlow,volFlow,resTime;
 	
 	//Flow Area calculation
 	flowArea=PI*diaInner*diaInner/4;
 	
+	//Volumetric flow
 	volFlow=massFlow/fluidDensity;
 	
+	//Flow velocity should be 1-3 m/s for liquids and 10-40 m/s for gases
 	flowVelocity=volFlow/flowArea;
 	
+	//Residence time - should be more than 1 sec to avoid computational leaks
 	resTime=pipeLength/flowVelocity;
 	
+	//fitResType -Fitting Resistance type - 0=2K method, 1=3K method, 2=L/D method (Refer Crane TP410)
+	fitResistance=1000.0;
 	
 }
