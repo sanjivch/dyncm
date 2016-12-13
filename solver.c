614cc88b2af65b89c07bf71bf746f152275bbc94
Sanjiv <sanjivch@users.noreply.github.com>
2016-12-13 15:43:05 +0530

    Create solver.c

---
+++ solver.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int arraySize, num_pipe, num_node;
+	float a[10][10],h[10],q[10];
+	float a12[10][10],a21[][];
+	float n;
+	float q_pipe[],k_pipe[],dE[];
+	float h_node[], dq[];
+	
+	//get the count of number of pipes and the number of nodes. Ignore boundaries!
+	arraySize = num_pipe + num_node;
+	
+	
+	
+}
