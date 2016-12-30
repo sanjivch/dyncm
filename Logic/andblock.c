ad4e7039c7486ec58ddd00c0a0f8c5810e0f6143
Sanjiv <sanjivch@users.noreply.github.com>
2016-12-30 14:36:55 +0530

    Create andblock.c

---
+++ Logic/andblock.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int numInputs,sumInputs, outResult;
+	
+	//if any input is FALSE, the output is FALSE (= 0)
+	if(sumInputs!=numInputs)
+		outResult=0;
+	else
+		outResult=1;
+}
