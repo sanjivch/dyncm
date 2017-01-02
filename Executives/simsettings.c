9df3e1f4d2284d1c5efb7897bb47885d7c6690ec
Sanjiv <sanjivch@users.noreply.github.com>
2017-01-02 16:05:28 +0530

    Update simsettings.c

--- Executives/simsettings.c
+++ Executives/simsettings.c
@@ -5,7 +5,15 @@ void main()
 {
 	float simTime, deltaT;
 	int executionOrder;//0 = Left to Right; 1 =Process order
+	int simRunFlag;// 0 = Pause; 1= Run; 2= Stop
 	
+	//Run a timer with a length of simTime. Increment with deltaT 
+	if(simRunFlag == 0)
+		//Pause the simulation. Should not update any values
+	else if(simRunFlag == 1)
+		//Update the values in each block depending on the scan time
+	else
+		//Stop the simulation. Prompt with a warning!
 	
 	
 }
