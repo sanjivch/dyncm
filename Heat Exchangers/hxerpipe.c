b1b8efa5d68205a186685d959013a09c43b759b3
Sanjiv <sanjivch@users.noreply.github.com>
2017-01-06 15:33:54 +0530

    Update hxerpipe.c

--- Heat Exchangers/hxerpipe.c
+++ Heat Exchangers/hxerpipe.c
@@ -9,22 +9,28 @@ void main()
 float diaInner, diaOuter, lenPipe;
 int numTubes;
 float areaHX, areaFinHX, LMTD, heatDuty, U, UcorrectionFactor, delT_In, delT_Out;
-
+int dutyCalcType;//Slave =0; Master =1; //Master drives the heat exchange- it calculates the duty, slave accepts the duty(-ve value)
+int configType; //0 = Co Current; 1 = Counter Current 
 
 areaHX = areaFinHX + numTubes * PI * diaOuter * lenPipe/ 4.0;
 
-delT_In =
-delT_Out =
+
+//delta T calculation changes depending on the configuration of heat exchanger 
+delT_In = T1In - T2In;
+delT_Out = T1Out - T2Out;
 
 LMTD = (delT_In-delT_Out)/ln(delT_In/delT_Out)
-
-heatDuty = U * areaHX * LMTD ;
+//Depending on the duty calculation type, the heatDuty changes
+if(dutyCalcType == 1)
+{
+	heatDuty = U * areaHX * LMTD ;
+}
+else
+	heatDuty = -getHeatDuty();//slave type
 
 T1Out = T1In + heatDuty/ (massFlow * heatCapacity);//heatDuty becomes negative for the other stream
 
 
-
-heatDuty = getHeatDuty();
 //UcorrectionFactor * 
 
 
