a70e9400e5d6321a34c74e65fe0f054e2ec2d9ef
Sanjiv <sanjivch@users.noreply.github.com>
2017-01-18 15:27:08 +0530

    Create worksheetsolver.c

---
+++ Solvers/worksheetsolver.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#includ <math.h>
+
+void main()
+{
+	//Worksheet Solver
+	
+	simTime = getSimTime();//get sim time from the executive; to be read only once per simulation
+	
+	//On Simulate
+	
+	onSimulate();
+	
+	//Read from the scan classes here
+	
+	//Execution order - flow or custom
+	
+	//Worksheet should should have the minimum timestep; fastest execution time
+	
+	
+}
