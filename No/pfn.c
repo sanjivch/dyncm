102b32cfc178d0622691b7edee47f055d7608901
Sanjiv <sanjivch@users.noreply.github.com>
2017-01-20 09:42:33 +0530

    Update pfn.c

--- No/pfn.c
+++ No/pfn.c
@@ -3,21 +3,29 @@
 
 void main()
 {
-	float elevation, initT, initP;
-	int initCompIndex;
+	float elevation, initT, initP;//GUI level parameters
+	int initCompIndex;//GUI level parameters 
 	float nodeTemp, nodeDensity, nodePressure;
 	
 	
 	//volumetric flows come directly from the solver- convert into mass flows
 	
-	//densities of the streams are obtained from getProperty() function
+	//densities of the inletPorts are obtained from getProperty() function
 	
-	for(int stream=1; stream==numStreams; stream++)
-		mass_density+=massFlowIn[stream]/densityIn[stream];
+	for(int inletPort=1; inletPort==numStreams; inletPort++)
+		mass_density+=massFlowIn[inletPort]/densityIn[inletPort];
 	
 	nodeDensity = massOut/mass_density;
 	
-	//nodeTemp = 
+	
+	//Temperature is calculated from the enthalpy H_mix = (Σ m_port * H_port)/massOut;
+	enthalpySum += massFlowIn[inletPort]*enthalpyIn[inletPort];
+	enthalpyMix = enthalpySum/massOut;
+	nodeTemp = (enthalpyMix/spHeat)+ refTemp;
+	
+	//Pressure is calculated from the solver -  convert into the appropriate units (kPa or psi or barg)
+	
+	
 	
 	
 }
