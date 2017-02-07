ba1eebc58336ec62aa8d626e8b71209c3c7e40a3
Sanjiv <sanjivch@users.noreply.github.com>
2017-02-07 09:44:01 +0530

    Update and.h

--- Logic/and.h
+++ Logic/and.h
@@ -1,19 +1,19 @@
 #ifndef AND_H
 #define AND_H
 
-#include "graphicelement.h"
+#include "unitoperation.h"//changed the graphicelement.h to unitoperation.h
 
-class And : public GraphicElement {
+class And : public UnitOperation {//change the name of the parent to UnitOperation from GraphicElement
 public:
   explicit And( QGraphicsItem *parent = 0 );
 
   /* GraphicElement interface */
 public:
-  virtual ElementType elementType( ) {
-    return( ElementType::AND );
+  virtual UOType uoType( ) {//changed ElelmentType to UOType and elementType() to uoType()
+    return( UOType::AND );
   }
-  virtual ElementGroup elementGroup( ) {
-    return( ElementGroup::GATE );
+  virtual UOGroup uoGroup( ) {//changed ElementGroup to UOGroup and elementGroup() to uoGroup()
+    return( UOGroup::GATE );
   }
   void updateLogic( );
 
