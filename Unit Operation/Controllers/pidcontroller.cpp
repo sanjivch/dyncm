7ad5f34e9a867bea030444dc149d5ccf0c11f0af
Sanjiv <sanjivch@users.noreply.github.com>
2017-04-25 10:41:38 +0530

    Update and rename pidcontroller.c to pidcontroller.cpp

--- Unit Operation/Controllers/pidcontroller.cpp
+++ Unit Operation/Controllers/pidcontroller.cpp
@@ -1,25 +1,26 @@
-#include <stdio.h>
-#include <conio.h>
+#include <iostream>
 #include <math.h>
+using namespace std;
 
-void main()
+int main()
 {
-
-	float processValue, setPoint, error, previousError=0.0, remSetPoint, controllerOut, epsilon=0.001, Kp, Ki, Kd, integral, derivative;//epsilon is the tolerance and is decided by the user
+	
+	double processValue, setPoint, error, previousError=0.0, remSetPoint, controllerOut;
+	double epsilon=0.001, Kp, Ki, Kd, integral, derivative;//epsilon is the tolerance and is decided by the user
 	int controllerType, controllerMode, controllerAction;
 	//controllerType: 0=P only; 1=PI; 2=PID;  3 PD - To be decided by the user
 	//controllerMode: 0=Manual; 1=Auto: 2=Remote
 	//controllerAction:0=Direct; 1=Reverse
-	float time, simTime=100.0, deltaT=0.01;//deltaT to be decided by the user
+	double tim, simtim=100.0, deltaT=0.01;//deltaT to be decided by the user
 	
 	
 	//Controller tuning parameters - defaults; User can tune the parameters to achieve satisfactory control 
 	Kp=1.0;
-	Ki=1.0;//Integral rate is used instead of time
+	Ki=1.0;//Integral rate is used instead of tim
 	Kd=1.0;
 	
 	
-	for(time=0.0; time=simTime; time+=deltaT)
+	for(tim=0.0; tim=simtim; tim+=deltaT)
 	{
 		error=setPoint-processValue;
 			
