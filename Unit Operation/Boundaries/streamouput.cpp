c2644600a6c67f7cfc837713f5bcc67edce99724
Sanjiv <sanjivch@users.noreply.github.com>
2017-04-25 10:36:30 +0530

    Update and rename streamouput.c to streamouput.cpp

---
+++ Unit Operation/Boundaries/streamouput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	double outletPres;
+	double elevation;
+	double fluidDensity;
+	
+	//Part of the solver. Flows will be defined based on the upstream objects!
+	//all this is object has to do is define the pressure of the boundary and receive the flow give by the upstream connector
+}
