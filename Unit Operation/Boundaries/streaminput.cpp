6126c48e4c8fc83a32732a769c93fb6570e69e0c
Sanjiv <sanjivch@users.noreply.github.com>
2017-04-25 10:34:34 +0530

    Update streaminput.cpp

--- Unit Operation/Boundaries/streaminput.cpp
+++ Unit Operation/Boundaries/streaminput.cpp
@@ -7,7 +7,20 @@ int main()
 	double elevation;
 	double fluidDensity;
 	int enableEquilibrium;//option helps in evaluating the saturated pressure or temp- may be for later- dummy variable
+	int numComponents;
+	cin >> numComponents;
+	double sumComp = 0.0;
+	double composition[numComponents];
 	
 	fluidDensity=1000.0;//default density is 1000 kg/m3. Fluid Density will be defined by the composition. Composition should be read from the Component db. Composition will in mass fraction or mol fraction
 	//Part of the solver. Flows will be defined based on the downstream objects!
+	
+	for(int i= 0; i< numComponents; i++){
+	  cin >> composition[i];
+	  sumComp+= composition[i];
+	}
+	cout << sumComp << endl;
+	//fluidDensity = 
+	
+	
 }
