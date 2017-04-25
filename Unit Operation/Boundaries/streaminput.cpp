4e77aa44cae55e9344a82d96908cb1a2f9e01c60
Sanjiv <sanjivch@users.noreply.github.com>
2017-04-25 09:49:05 +0530

    Update and rename streaminput.c to streaminput.cpp

--- Unit Operation/Boundaries/streaminput.cpp
+++ Unit Operation/Boundaries/streaminput.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <conio.h>
+#include <iostream>
+using namespace std;
 
-void main()
+int main()
 {
-	float inletPres, inletTemp;
-	float elevation;
-	float fluidDensity;
+	double inletPres, inletTemp;
+	double elevation;
+	double fluidDensity;
 	int enableEquilibrium;//option helps in evaluating the saturated pressure or temp- may be for later- dummy variable
 	
 	fluidDensity=1000.0;//default density is 1000 kg/m3. Fluid Density will be defined by the composition. Composition should be read from the Component db. Composition will in mass fraction or mol fraction
