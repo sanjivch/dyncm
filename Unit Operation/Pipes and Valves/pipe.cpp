34b94f0e416bf10c77b0fcbc36fc1bf780e6a8cc
Sanjiv <sanjivch@users.noreply.github.com>
2017-05-11 09:35:21 +0530

    Update pipe.cpp

--- Unit Operation/Pipes and Valves/pipe.cpp
+++ Unit Operation/Pipes and Valves/pipe.cpp
@@ -1,11 +1,21 @@
+// 	Variable definitions:
+//  	diaInner,diaOuter,epsilon,pipeLength, fitResistance, K1, Kinf, NRe;
+//	viscosity, refViscosity, opTemp, refTemp;
+//	fitResType;
+//	flowArea;
+//	flowVelocity,fluidDensity,massFlow,volFlow,resTime;
+
+
 #include <iostream>
 #include <math.h>
 #define PI 3.1415
 
+using namespace std;
+
 int main()
 {
-	double diaInner,diaOuter,epsilon,pipeLength, fitResistance, K1, Kinf, NRe;
-	double viscosity, refViscosity, opTemp,refTemp;
+	double diaInner,diaOuter,epsilon,pipeLength, fitResistance, K1, Kinf, fricFactor, NRe;
+	double viscosity, refViscosity, opTemp, refTemp;
 	int fitResType;
 	double flowArea;
 	double flowVelocity,fluidDensity,massFlow,volFlow,resTime;
@@ -22,17 +32,37 @@ int main()
 	//Residence time - should be more than 1 sec to avoid computational leaks
 	resTime = pipeLength/flowVelocity;
 	
+	
+	//Viscosity needs to be calculated at the operating tempwerature from the power law mu/muo=(T/To)^0.7 
+	//Ref: http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
+	viscosity = refViscosity*pow((opTemp/refTemp),0.7);
+	
 	//Reynolds number
-	//Viscosity needs to be calculated at the operating tempwerature from the power law mu/muo=(T/To)^0.7 Ref: http://www-mdp.eng.cam.ac.uk/web/library/enginfo/aerothermal_dvd_only/aero/fprops/propsoffluids/node5.html
-	viscosity = refViscosity*(opTemp/refTemp)^0.7;
 	NRe = diaInner*flowVelocity*fluidDensity/viscosity;
 	
-	//fitResType -Fitting Resistance type - 0=2K method, 1=3K method, 2=L/D method (Ref: Crane TP410)
-	fitResistance=1000.0;
+	cin >> fitResType;
+	//fitResType -Fitting Resistance type:
+	//      0 = 2K method
+	//      1 = 3K method
+	//      2 = L/D method (Ref: Crane TP410)
+	// Ref: Chemwork - http://www.chemwork.org/PDF/board/Select%20the%20Best%20Fitting%20Pressure%20Loss%20Correlation.pdf
+	switch(fitResType) {
+      case 0 :
+         fitResistance = K1/NRe +Kinf*(1+1/diaInner);
+         break;
+     case 1 :
+         fitResistance = K1/NRe +Kinf*(1+Kinf/pow(diaInner,0.3));
+         break;
+     case 2 :
+         fricFactor = 16.0/NRe;
+	 fitResistance = K1 * fricFactor;
+         break;
+     default :
+        cout << "Invalid Fitting Resistance type" << endl;
+   }
+	cout << fitResistance <<endl;
 	
-	fitResistance = K1/NRe +Kinf(1+1/diaInner);//2K method
+	
+	//2K method
 	//In this Equation K∞ is the "classic" K for a large fitting in the fully turbulent flow regime and K1 is the resistance coefficient at a Reynolds Number of 1. Note that although the K's and Re are dimensionless the fitting inside diameter (D) must be given in inches. Ref: http://www.katmarsoftware.com/articles/pipe-fitting-pressure-drop.htm
-	
-	
-	
 }
